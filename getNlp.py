fdecf3a551477296173ca287c6bdf40baaba0727
Isogami <u2.isogami@gmail.com>
2018-08-13 18:37:03 +0900

    ネガポジ
    MeCab 形態素解析
    Selenium webdriver(Chrome) 対義語をweblioから引っ張ってくる
    wxPython (GUI)

--- getNlp.py
+++ getNlp.py
@@ -16,39 +16,23 @@ __location__ = os.path.realpath(
                                 os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def nlp(data):
-    abc = ""
+    result = ""
     nm = MeCab() # nmというMeCabクラスのインスタンスを作成
-    #points = 0 # 文章全体の評価
     negaposi_dic = getNegaPosiDic() # 評価データの読み込み(さっき作った関数を呼び出している。)
     sentenses = re.split("[。！!♪♫★☆>?？（）w]", data)  # 一文ごとに分ける
-    #try:
     for sentense in sentenses: # 文の数だけ繰り返す
         negaposi = 0
         result_all = nm.parse(sentense) # 形態素解析して品詞分解をしている。
         result_words = result_all.split("\n")[:-1]  # 単語ごとに分ける
         for word in result_words:
-            #try:
             word_toarray = re.split('[\t,]', word)
             if word_toarray[7] in negaposi_dic and int(negaposi_dic[word_toarray[7]]) < 0:
-                #negaposi = int(negaposi_dic[word_toarray[7]])  # その文のネガポジ
-                #print(word_toarray[7],negaposi_dic[word_toarray[7]], flush=True) # 評価リストに入っていたワードとその評価
-                #if negaposi < 0:
                 url = "https://thesaurus.weblio.jp/antonym/content/{0}".format(word_toarray[7])
                 driver.get(url)
-                abc += driver.find_element_by_class_name('wtghtAntnm').text
-                #else:
-                #    abc += word_toarray[7]
+                result += driver.find_element_by_class_name('wtghtAntnm').text
                 else:
-                    abc += word_toarray[7]
-#except Exception as e:
-#print('%r' % e, flush=True)
-#print()
-#points += negaposi # 文章全体の評価に加算
-#except Exception as e:
-#print('%r' % e, flush=True)
-#print(data, flush=True)
-#print()
-return abc # 文章全体の値を返す。
+                    result += word_toarray[7]
+    return result
 
 def getNegaPosiDic():
     with codecs.open(os.path.join(__location__, "./dataset/yougen.csv"), 'r', 'utf-8') as f_in:
@@ -72,14 +56,15 @@ def click_togglebutton(event):
 
 def enter_textctrl(event):
     if t_button.GetValue() == True:
-        s_text_3.SetLabel('切り替え完了！')
         input_textctrl = text_1.GetValue()
         output_textctrl = nlp(input_textctrl)
         text_2.SetLabel(output_textctrl)
+        s_text_3.SetLabel('切り替え完了！')
     else:
-        s_text_3.SetLabel('切り替えＯＦＦ')
         text_2.SetLabel(text_1.GetValue())
+        s_text_3.SetLabel('切り替えＯＦＦ')
 
+#GUI生成部分
 application = wx.App()
 frame = wx.Frame(None, wx.ID_ANY, 'ネガポジ切り替え', size=(800, 400))
 
@@ -88,10 +73,11 @@ panel = wx.Panel(frame, wx.ID_ANY)
 s_text_1 = wx.StaticText(panel, wx.ID_ANY, 'スイッチ')
 t_button = wx.ToggleButton(panel, wx.ID_ANY, 'OFF')
 t_button.SetValue(False)
-t_button.Bind(wx.EVT_TOGGLEBUTTON, click_togglebutton)
+
+t_button.Bind(wx.EVT_TOGGLEBUTTON, click_togglebutton) #ボタンが押されたら，click_togglebuttonを呼び出す
 s_text_2 = wx.StaticText(panel, wx.ID_ANY, '文章を入力して下さい')
 text_1 = wx.TextCtrl(panel, wx.ID_ANY, style=wx.TE_PROCESS_ENTER)
-text_1.Bind(wx.EVT_TEXT_ENTER, enter_textctrl)
+text_1.Bind(wx.EVT_TEXT_ENTER, enter_textctrl) #ボタンが押されたら，enter_textctrlを呼び出す
 s_text_3 = wx.StaticText(panel, wx.ID_ANY, '　　　　　　　')
 s_text_4 = wx.StaticText(panel, wx.ID_ANY, '出力文')
 text_2 = wx.TextCtrl(panel, wx.ID_ANY)
@@ -108,25 +94,3 @@ panel.SetSizer(layout)
 
 frame.Show()
 application.MainLoop()
-
-
-
-# options = webdriver.chrome.options.Options()
-# options.add_argument("--headless")  # これ消せばブラウザ画面が出ます
-#
-# driver = webdriver.Chrome(chrome_options=options)
-#
-# __location__ = os.path.realpath(
-#     os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-
-
-# if __name__=='__main__':
-#     data = ""
-#     while True:
-#         t = input()
-#         if t == '':
-#             break
-#         data = data + str(t)
-#     print(data)
-#     print(nlp(data))
